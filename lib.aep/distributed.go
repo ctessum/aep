9eb145b106b9146c56b74b8851f84856ce5cac1f
ctessum <ctessum@gmail.com>
2013-12-12 20:16:54 -0600

    minor changes

--- lib.aep/distributed.go
+++ lib.aep/distributed.go
@@ -11,15 +11,10 @@ var RPCport = "6061" // Port for RPC communications for distributed computing
 
 // Set up a server to remotely calculate gridding surrogates.
 func DistributedServer(PGc gis.PostGISconnecter) {
-	srgsInGridCell, err := gis.NewSrgsInGridCellCalculator(PGc)
-	if err != nil {
-		panic(err)
-	}
 	srgGenWorker, err := gis.NewSrgGenWorker(PGc)
 	if err != nil {
 		panic(err)
 	}
-	rpc.Register(srgsInGridCell)
 	rpc.Register(srgGenWorker)
 	rpc.HandleHTTP()
 	l, err := net.Listen("tcp", ":"+RPCport)
