963f785c824e1494a3119fb1f82130bc05ea548a
Chris Tessum <ctessum@gmail.com>
2017-05-31 15:12:46 -0700

    Make shapefile null detection more flexible.

--- surrogate.go
+++ surrogate.go
@@ -434,7 +434,7 @@ func (srg *SrgSpec) getSrgData(gridData *GridDef, tol float64) (*rtree.Rtree, er
 					for i, name := range srg.WeightColumns {
 						var v float64
 						vstring := data[name]
-						if vstring == "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" || vstring == "****************" {
+						if strings.Contains(vstring, "\x00\x00\x00\x00\x00\x00") || strings.Contains(vstring, "***") {
 							// null value
 							v = 0.
 						} else {
